5239a9cc85f83012a997e08cba4ec6392589ac1d
sophie chen <pandoaki@gmial.com>
2019-08-10 15:12:16 +0800

    new version

--- read.py
+++ read.py
@@ -6,9 +6,15 @@ with open('reviews.txt', 'r') as f:
 		count += 1 #count = count + 1
 		if count % 1000 == 0: # %: 求餘數。
 			print(len(data),'/1000000')
-print(data[0])
-print('--------------------------')
-print(data[999999])		
+print('檔案讀取完了,總共有', len(data), '筆資料。')
+#算留言平均長度：
+sum_len = 0
+for d in data:
+	sum_len += len(d) #sum_len = sum_len +len(d)
+print('平均每筆留言字數為', sum_len / len(data), '字。')
+
+
+
 
 
 
